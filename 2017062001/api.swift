682bfe9bd272e1f6ade50c0785ec7028248c2380
iii-user <iii-user@iii-userde-MacBook-4.local>
2017-06-20 12:01:01 +0800

    F

---
+++ 2017062001/api.swift
@@ -0,0 +1,17 @@
+//
+//  api.swift
+//  2017062001
+//
+//  Created by iii-user on 2017/6/20.
+//  Copyright © 2017年 iii-user. All rights reserved.
+//
+
+import Foundation
+class BradAPI{
+    static func createAnswer()->String{
+        return "123"
+    }
+    static func checkAB(answer: String, guess: String)->String {
+        return "1A2B"
+    }
+}
